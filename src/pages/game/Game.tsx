9bc076859d8518c8131a548817467cd24e0bcd21
junpang-bob <junpang1027@163.com>
2024-05-15 17:03:35 +0800

    update css

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -1,11 +1,12 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, } from 'react-router-dom'
 import styles from './Game.module.scss'
 export default function Game() {
   return (
     <>
       <div className={styles.gameContext}>
         <div className={styles.gameList}>
-          <div>九宫格</div>
+          <div style={{marginBottom:'10px'}}>九宫格</div>
+          <div style={{marginBottom:'10px'}}>mpGame</div>
         </div>
         <div className={`${styles.gameContainer} lemon_center`}>
           <Outlet />
