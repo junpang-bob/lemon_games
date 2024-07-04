c9eb1046576d6eb6275bebaeb48ad124431a8d0f
junpang-bob <junpang1027@163.com>
2024-07-04 15:05:56 +0800

    fail 了

--- src/pages/game/GreedySnake/GreedySnake.tsx
+++ src/pages/game/GreedySnake/GreedySnake.tsx
@@ -42,35 +42,36 @@ export default function GreedySnake() {
       });
     })
   }, [])
-  useEffect(() => {
-    console.log('test1');
+  // useEffect(() => {
+  //   console.log('test1');
 
-    const timer = setInterval(() => {
-      setHeader((header) => {
-        if (direction === 'ArrowDown') {
-          return { top: header.top + 5, left: header.left, rotate: 'rotate(180deg)' }
-        }
-        else if (direction === 'ArrowUp') {
-          return { top: header.top - 5, left: header.left, rotate: 'rotate(0deg)' }
-        }
-        else if (direction === 'ArrowRight') {
-          return { top: header.top, left: header.left + 5, rotate: 'rotate(90deg)' }
-        }
-        else if (direction === 'ArrowLeft') {
-          return { top: header.top, left: header.left - 5, rotate: 'rotate(270deg)' }
-        }
-        return { top: 0, left: 0, rotate: 'rotate(90deg)' }
-      })
-    }, 50)
-    return () => {
-      console.log('测试');
-      return clearInterval(timer)
-    }
-  }, [direction])
+  //   const timer = setInterval(() => {
+  //     setHeader((header) => {
+  //       if (direction === 'ArrowDown') {
+  //         return { top: header.top + 5, left: header.left, rotate: 'rotate(180deg)' }
+  //       }
+  //       else if (direction === 'ArrowUp') {
+  //         return { top: header.top - 5, left: header.left, rotate: 'rotate(0deg)' }
+  //       }
+  //       else if (direction === 'ArrowRight') {
+  //         return { top: header.top, left: header.left + 5, rotate: 'rotate(90deg)' }
+  //       }
+  //       else if (direction === 'ArrowLeft') {
+  //         return { top: header.top, left: header.left - 5, rotate: 'rotate(270deg)' }
+  //       }
+  //       return { top: 0, left: 0, rotate: 'rotate(90deg)' }
+  //     })
+  //   }, 50)
+  //   return () => {
+  //     console.log('测试');
+  //     return clearInterval(timer)
+  //   }
+  // }, [direction])
 
   return <div className="relative w-[400px] h-[400px] bg-slate-500">
-    <SnakeBody positionLeft={next2.left} positionTop={next2.top} rotate={next2.rotate} />
+    失败了，不知道咋写好了
+    {/* <SnakeBody positionLeft={next2.left} positionTop={next2.top} rotate={next2.rotate} />
     <SnakeBody positionLeft={next1.left} positionTop={next1.top} rotate={next1.rotate} />
-    <div ref={snakeRef} className='snake-header' style={{ top: `${header.top}px`, left: `${header.left}px`, transform: header.rotate }}>猴</div>
+    <div ref={snakeRef} className='snake-header' style={{ top: `${header.top}px`, left: `${header.left}px`, transform: header.rotate }}>猴</div> */}
   </div>
 }
