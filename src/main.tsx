311d29efd57ae166c7c4ae018f892a6db1b5b38f
junpang <junpang1027@163.com>
2024-09-29 13:07:05 +0800

    del some code

--- src/main.tsx
+++ src/main.tsx
@@ -1,63 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
 import {
-  RouterProvider,
-  createHashRouter
+	RouterProvider,
+	createHashRouter
 } from "react-router-dom";
 import './main.css'
 import Game from './pages/game/Game';
 import XOGame from './pages/game/XOGame/XOGame';
-import MpGame from './pages/game/MpGame/MpGame';
 import './assets/css/base.css'
 import './assets/css/layout.css'
-import AddGame from './pages/game/AddGame/AddGame';
 import BubblesGame from './pages/game/BubblesGame/BubblesGame';
-import TDGame from './pages/game/TDGame/TDGame';
 import Home from './pages/home/Home';
-import GreedySnake from './pages/game/GreedySnake/GreedySnake';
 
 
 const router = createHashRouter([
-  {
-    path: '/',
-    element: <Home />
+	{
+		path: '/',
+		element: <Home />
 
-  },
-  {
-    path: "/game",
-    element: <Game />,
-    children: [
-      {
-        path: 'xogame',
-        element: <XOGame />
-      }, {
-        path: 'mpgame',
-        element: <MpGame />
-      }, {
-        path: 'addGame',
-        element: <AddGame />
-      },
-      {
-        path: 'bubblesGame',
-        element: <BubblesGame />
-      },
-      {
-        path: 'tdGame',
-        element: <TDGame />
-      },
-      {
-        path: 'greedySnake',
-        element: <GreedySnake />
-      },
-
-    ]
-  },
+	},
+	{
+		path: "/game",
+		element: <Game />,
+		children: [
+			{
+				path: 'xogame',
+				element: <XOGame />
+			},
+			{
+				path: 'bubblesGame',
+				element: <BubblesGame />
+			},
+		]
+	},
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <div className=' bg-black  text-white w-[100vw] h-[100vh] overflow-scroll'>
-      <RouterProvider router={router} />
-    </div>
-  </React.StrictMode>,
+	<React.StrictMode>
+		<div className=' bg-black  text-white w-[100vw] h-[100vh] overflow-scroll'>
+			<RouterProvider router={router} />
+		</div>
+	</React.StrictMode>,
 )
