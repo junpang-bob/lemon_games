5ab29b8b07f6c5653933114520098f8ec95aed56
junpang-bob <junpang1027@163.com>
2025-03-31 13:44:12 +0800

    update

--- src/pages/demos/pixiTag/pixiTag.tsx
+++ src/pages/demos/pixiTag/pixiTag.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react';
-import { Application, Container } from 'pixi.js';
+import { Application, Container, Sprite, Assets } from 'pixi.js';
 
 function PixiComponent() {
   const canvasRef = useRef<HTMLDivElement>(null);
@@ -23,6 +23,31 @@ function PixiComponent() {
           height: 40,
         });
         app.stage.addChild(container);
+        const texture = await Assets.load('https://pixijs.com/assets/bunny.png');
+
+        // Create a 5x5 grid of bunnies in the container
+        for (let i = 0; i < 25; i++) {
+          const bunny = new Sprite(texture);
+
+          bunny.x = (i % 5) * 40;
+          bunny.y = Math.floor(i / 5) * 40;
+          container.addChild(bunny);
+        }
+
+        // Move the container to the center
+        container.x = app.screen.width / 2;
+        container.y = app.screen.height / 2;
+
+        // Center the bunny sprites in local container coordinates
+        container.pivot.x = container.width / 2;
+        container.pivot.y = container.height / 2;
+
+        // Listen for animate update
+        app.ticker.add((time) => {
+          // Continuously rotate the container!
+          // * use delta to create frame-independent transform *
+          container.rotation -= 0.01 * time.deltaTime;
+        });
         canvasRef.current.appendChild(app.canvas as HTMLCanvasElement);
       }
       console.log(app);
@@ -30,11 +55,11 @@ function PixiComponent() {
 
     initPixi();
 
-    return () => {
-      if (appRef.current) {
-        appRef.current.destroy(true);
-      }
-    };
+    // return () => {
+    //   if (appRef.current) {
+    //     appRef.current.destroy(true);
+    //   }
+    // };
   }, []);
 
   return <div ref={canvasRef} className="border-2 border-gray-300 rounded-lg shadow-lg" />;
