b8bd9c6822197b1f05f329832941832c1590c923
junpang-bob <junpang1027@163.com>
2025-07-01 10:34:28 +0800

    update

--- src/pages/demos/Demos.tsx
+++ src/pages/demos/Demos.tsx
@@ -1,9 +1,11 @@
 import { Link, Outlet } from 'react-router-dom'
 
 export default function Demos() {
+
+  const isProduction = import.meta.env.PROD
   return (
     <div>
-      <Link to="/home/demos/markets">markets</Link>
+      {!isProduction ? <Link to="/home/demos/markets">markets</Link> : null}
       <Outlet />
       {/* {test} */}
     </div>
